513fe534d04e1fc1fed1b79583b033830d5f2c8d
changkyu <jinchangkyu@gmail.com>
2019-09-27 11:40:48 +0900

    Initial commit

---
+++ src/com/nuri/t2/Eat.java
@@ -0,0 +1,27 @@
+package com.nuri.t2;
+
+public class Eat implements Runnable{
+	
+	@Override
+	public void run() {
+
+		this.lunch();
+	}
+	
+	public void lunch() {
+		
+		for(int i=0; i<10; i++) {
+			
+			try {
+				Thread.sleep(500);
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("촵촵");
+		}
+	}
+
+}
---
+++ src/com/nuri/t2/Main.java
@@ -0,0 +1,17 @@
+package com.nuri.t2;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Eat eat = new Eat();
+		Tv tv = new Tv();
+		
+		Thread t1 = new Thread(eat);
+		Thread t2 = new Thread(tv);
+		
+		t1.start();
+		t2.start();
+	}
+
+}
